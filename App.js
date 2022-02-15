bf54c969d90f69c1e2bb71e462ca0f9b0ff1f942
Vashu473 <vmanishkumar04@gmail.com>
2022-02-15 23:37:05 +0530

    changed everything in app.js file

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React from "react";
+const App = () => {
+  Alert.alert("Danger", "Your storage is running out", [
+    { text: "Okay", style: "default" },
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.div}>
+      <Text style={styles.p}>App</Text>
     </View>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
-  container: {
+  div: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  p: {
+    fontSize: 28,
+    color: "#ffffff",
+    fontStyle: "italic",
   },
 });
